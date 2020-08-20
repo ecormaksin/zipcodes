d2bee4e74d57277f3c5ca124efcb8e96f388b562
ecormaksin <truth_jp_4133@yahoo.co.jp>
2020-08-20 21:45:57 +0900

    アノテーションの調整

--- infra/src/test/java/com/example/zipcodes/infra/db/repository/PrefectureRepositoryImplTest.java
+++ infra/src/test/java/com/example/zipcodes/infra/db/repository/PrefectureRepositoryImplTest.java
@@ -6,11 +6,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.zipcodes.infra.db.jpa.view.Prefecture;
@@ -23,9 +20,7 @@ class PrefectureRepositoryImplTest {
 	@Autowired
 	private PrefectureRepositoryImpl prefectureRepositoryImpl;
 
-	@SpringBootApplication(scanBasePackages = {"com.example.zipcodes.infra"})
-	@EntityScan(basePackages = {"com.example.zipcodes.infra.db.jpa.view"})
-	@EnableJpaRepositories(basePackages = {"com.example.zipcodes.infra.db.jpa.repository"})
+	@CustomJpaTest
 	static class TestConfiguration {
 	}
 
